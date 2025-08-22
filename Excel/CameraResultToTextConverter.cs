9e0bc2f96fdfea8588fc98fa48924eab6b9d25b1
tomasz <tomasz_starek@jabil.com>
2025-08-22 10:35:27 +0200

    płyta: zła/dobra added value converter

---
+++ Excel/CameraResultToTextConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wiring
+{
+    public class CameraResultToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool cameraResult)
+                return cameraResult ? "Płyta: DOBRA" : "Płyta: ZŁA";
+
+            return "Płyta: nieznana";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
